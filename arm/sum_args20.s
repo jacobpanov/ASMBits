449b9fe005f3af94fe46d990dcd63c41603d417e
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-06-24 23:38:38 +0000

    Preliminary sum_args20.s coding

--- arm/sum_args20.s
+++ arm/sum_args20.s
@@ -5,36 +5,47 @@
 
 .global _start
 _start:
-    mov r0, #1
-    mov r1, #2
-    mov r2, #3
-    mov r3, #4
-    // push remaining 16 numbers onto the stack (values 5..20)
-    mov r4, #20
-    mov r5, #16
-1:
-    push {r4}
-    sub r4, r4, #1
-    subs r5, r5, #1
-    bne 1b
-    bl sum
-    add sp, sp, #64      // clean 16 params
-    1: b 1b
+	ldr sp, =0x04000000	// Initial sp
+	sub sp, sp, #64	// 16 stack parameters
+	mov r0, #5
+	mov r1, #20
+	mov r2, sp
+initloop:
+	str r0, [r2], #4       // Post-indexed addressing: Add 4 to r2 after
+	add r0, r0, #1
+	cmp r0, r1
+	ble initloop
 
-.text
-.global sum
+	mov r0, #1
+	mov r1, #2
+	mov r2, #3
+	mov r3, #4
+	
+	bl sum
+	add sp, sp, #64	// Clean up stack
+	1: b 1b  // done
+
 sum:
-    push {r4-r7, lr}
-    // save register params to stack
-    push {r0-r3}
-    mov r4, sp           // pointer to all 20 params
-    mov r5, #20
-    mov r0, #0
-1:
-    ldr r6, [r4], #4
-    add r0, r0, r6
-    subs r5, r5, #1
-    bne 1b
-    add sp, sp, #16      // drop saved regs
-    pop {r4-r7, lr}
+    push {r4, r5, r6, r7}      // Save callee-saved registers
+    sub sp, sp, #16            // Make space for 4 words
+    str r0, [sp, #0]           // Store r0 at lowest address
+    str r1, [sp, #4]           // Store r1
+    str r2, [sp, #8]           // Store r2
+    str r3, [sp, #12]          // Store r3
+
+    mov r4, sp                 // r4 = pointer to n1 (lowest address)
+    mov r5, #0                 // r5 = sum
+    mov r6, #0                 // r6 = loop counter
+
+sum_loop:
+    ldr r7, [r4, r6, lsl #2]   // Load argument (each is 4 bytes)
+    add r5, r5, r7             // Add to sum
+    add r6, r6, #1             // Increment counter
+    cmp r6, #20                // 20 arguments total
+    blt sum_loop
+
+    mov r0, r5                 // Return value in r0
+    add sp, sp, #16            // Remove the 4 pushed arguments
+    pop {r4, r5, r6, r7}       // Restore callee-saved registers
     bx lr
+
