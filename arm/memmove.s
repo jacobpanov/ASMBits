b52af7f6c328370a48da06cb887b2837ca880326
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-06-26 22:05:23 -0400

    Fixing memmove.s bug

--- arm/memmove.s
+++ arm/memmove.s
@@ -18,34 +18,23 @@ _start:
 
 .global memmove
 memmove:
-    push {r4-r5, lr}
     cmp r2, #0
-    beq finish
+    bxeq lr
     cmp r0, r1
-    beq finish
-    mov r3, r0          // save dest pointer
-    add r4, r1, r2      // end of source
-    cmp r0, r1
-    blt forward_copy
-    cmp r0, r4
-    bge forward_copy
-    // overlapping copy backward
-    add r1, r1, r2
+    blo copy_forward
     add r0, r0, r2
-1:
+    add r1, r1, r2
+
+copy_back:
+    ldrb r3, [r1, #-1]!
+    strb r3, [r0, #-1]!
     subs r2, r2, #1
-    ldrb r5, [r1, #-1]!
-    strb r5, [r0, #-1]!
-    bne 1b
-    mov r0, r3
-    b finish
-forward_copy:
-1:
-    ldrb r5, [r1], #1
-    strb r5, [r0], #1
+    bne copy_back
+    bx lr
+    
+copy_forward:
+    ldrb r3, [r1], #1
+    strb r3, [r0], #1
     subs r2, r2, #1
-    bne 1b
-    mov r0, r3
-finish:
-    pop {r4-r5, lr}
+    bne copy_forward
     bx lr
