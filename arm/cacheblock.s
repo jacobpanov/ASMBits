2673bc6f5987133e0dd59f588e1caff46b182de1
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-06-29 19:15:23 +0000

    Finishing cacheblock.s

--- arm/cacheblock.s
+++ arm/cacheblock.s
@@ -10,3 +10,10 @@ _start:
 1:      b 1b    // Done
 
 cacheblock:
+        // r0 = addr1, r1 = addr2
+        mov r2, r0, lsr #4     // r2 = addr1 >> 4
+        mov r3, r1, lsr #4     // r3 = addr2 >> 4
+        cmp r2, r3
+        moveq r0, #1           // If equal, return 1
+        movne r0, #0           // Else, return 0
+        bx lr
