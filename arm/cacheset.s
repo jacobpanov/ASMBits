b3ab3c68a6d522b5d05ffce545e8869e00bfa2cb
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-06-29 19:16:46 +0000

    Finishing cacheset.s

--- arm/cacheset.s
+++ arm/cacheset.s
@@ -10,3 +10,12 @@ _start:
 1:      b 1b    // Done
 
 cacheset:
+        // r0 = addr1, r1 = addr2
+        mov r2, r0, lsr #4         // r2 = addr1 >> 4
+        mov r3, r1, lsr #4         // r3 = addr2 >> 4
+        and r2, r2, #0xFF          // r2 = set index of addr1
+        and r3, r3, #0xFF          // r3 = set index of addr2
+        cmp r2, r3
+        moveq r0, #1
+        movne r0, #0
+        bx lr
