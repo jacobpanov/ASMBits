8dbcfc75fec6abb9a2aa5a66c671d0394f19b0b8
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-06-29 19:13:52 +0000

    Finishing uartgets.s

--- arm/uartread.s
+++ arm/uartread.s
@@ -21,3 +21,27 @@ _start:
 
 
 uartgets:
+        push {r1-r4, lr}           // Save registers we'll use
+        mov r1, r0                 // r1 = output pointer
+        mov r2, #0                 // r2 = length
+
+uartgets_loop:
+        bl read_uart               // r0 = UART status/char
+        tst r0, #(1 << 15)         // Check if char valid (bit 15)
+        beq uartgets_loop          // If not valid, poll again
+
+        and r3, r0, #0xFF          // r3 = received char (bits 7:0)
+        cmp r3, #0x0A              // Compare with '\n' (0x0A)
+        beq uartgets_done
+        cmp r3, #0x0D              // Compare with '\r' (0x0D)
+        beq uartgets_done
+
+        strb r3, [r1], #1          // Store char, increment pointer
+        add r2, r2, #1             // Increment length
+        b uartgets_loop
+
+uartgets_done:
+        mov r3, #0
+        strb r3, [r1]              // Null-terminate string
+        mov r0, r2                 // Return length in r0
+        pop {r1-r4, pc}            // Restore regs and return
