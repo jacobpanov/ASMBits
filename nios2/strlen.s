0fd4c9a5e4e1cdf334c7c14da2ceff2e0c6ce8ee
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-07-12 17:10:53 -0400

    Complete strlen.s

--- nios2/strlen.s
+++ nios2/strlen.s
@@ -13,4 +13,12 @@ _start:
 
 .global strlen
 strlen:
-    # Implement strlen here.
+    mov     r2, r0           # r2 = 0 (initialize length)
+strlen_loop:
+    ldbu    r3, 0(r4)        # load byte from [r4] into r3
+    beq     r3, r0, strlen_done  # if byte == 0, end of string
+    addi    r2, r2, 1        # increment length
+    addi    r4, r4, 1        # move to next byte
+    br      strlen_loop      # repeat
+strlen_done:
+    ret                      # length in r2
