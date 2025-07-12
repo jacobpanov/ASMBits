681d3dd8c42a18a78ebb3658ebe77e5ed2e70be6
Jacob Panov <84049465+jacobpanov@users.noreply.github.com>
2025-07-12 17:12:26 -0400

    Complete swap.s

--- nios2/swap.s
+++ nios2/swap.s
@@ -12,4 +12,19 @@ _start:
 
 # Swap two array elements
 swap:
-    # Implement swap here.
+    # Calculate the address of the first element: r4 + r5 * 4
+    slli    r7, r5, 2      # r7 = r5 * 4 (offset for index1)
+    add     r7, r4, r7     # r7 = pointer + offset (address of element 1)
+
+    # Calculate the address of the second element: r4 + r6 * 4
+    slli    r8, r6, 2      # r8 = r6 * 4 (offset for index2)
+    add     r8, r4, r8     # r8 = pointer + offset (address of element 2)
+
+    # Swap the values at these addresses
+    ldw     r9, 0(r7)      # r9 = *element1
+    ldw     r10, 0(r8)     # r10 = *element2
+
+    stw     r10, 0(r7)     # *element1 = *element2
+    stw     r9, 0(r8)      # *element2 = *element1
+
+    ret
